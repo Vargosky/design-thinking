e338125f8b7b51358a7bf55fc215383648c75211
Vargosky <sarem.vargas@gmail.com>
2024-06-02 12:54:24 -0400

    new

--- src/app/components/carousel/Component.js
+++ src/app/components/carousel/Component.js
@@ -3,16 +3,17 @@ import { Carousel } from "flowbite-react";
 import Image from "next/image";
 import { Tarjeta } from "../tarjeta/Tarjeta";
 import imagenes from "./imagenes/vercel.svg"
+import img1 from "./imagenes/pastel-de-choclo.jpg"
 
 export function Component() {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 w-5/6 centrado">
       <Carousel leftControl="left" rightControl="right">
-        <Image src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." width={500} height={500} />
+        <Image src={img1} alt="..." width={500} height={500} />
         <Tarjeta titulo={"esta es el titulo"} imagen={imagenes}/>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <Image src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." width={500} height={500} />
+        <Image src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." width={500} height={500} />
+        <Image src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." width={500} height={500} />
       </Carousel>
     </div>
   );
